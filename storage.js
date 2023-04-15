cd358514729f24ac5a776446149d9e899b6f0978
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-04-16 01:42:14 +0530

    Q13-DeletedatafromLocalStorage

--- storage.js
+++ storage.js
@@ -3,8 +3,10 @@ const email = document.getElementById('email');
 // const userName = document.getElementById('name').value;
 // console.log(userName)
 const form = document.getElementById('my-form');
+const ul = document.getElementById('users');
 // static uniqueId = 0;
 form.addEventListener('submit', storeAsObject);
+ul.addEventListener('click',removeItemFromUI);
 
 function storeData(e){
 
@@ -31,7 +33,29 @@ function storeAsObject(e){
         emailUser
     };
     let myObj_serialized = JSON.stringify(myObj);
+    const item = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.textContent = 'Delete';
+    btn.classList = 'Button';
+    item.appendChild(document.createTextNode(nameUser + '-' + emailUser));
+    item.appendChild(btn);
+    ul.appendChild(item);
+
     localStorage.setItem(emailUser , myObj_serialized);
     userName.value = '';
     email.value = '';
+}
+
+
+function removeItemFromUI(e){
+    e.preventDefault();
+    // console.log(userName.value.type);
+    // console.log(JSON.stringify(userName.value));
+
+    if(e.target.classList.contains('Button')){
+        let text = e.target.parentNode.firstChild.textContent;
+        let mailId = text.split('-')[1];
+        localStorage.removeItem(mailId);
+        ul.removeChild(e.target.parentNode);
+    }
 }
