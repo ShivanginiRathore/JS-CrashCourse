f84bef2c16fa0fa05c03ef8b37866218089bdc3c
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-04-28 00:24:19 +0530

    Q19-DeleteRequest

--- storage.js
+++ storage.js
@@ -29,25 +29,16 @@ allStorage();
 // QUESTION 11
 function storeAsObject(e){
     e.preventDefault();
-    let nameUser = userName.value;
-    let emailUser = email.value;
-    // let ran = Math.floor(Math.random() * 100)
-    let myObj={
-        nameUser,
-        emailUser
-    };
-    let myObj_serialized = JSON.stringify(myObj);
+    
     // Network call
     axios.post("https://crudcrud.com/api/bef2f6a52423441aa70259abb9574616/appintmentData",myObj)
     .then(data => {
-        showDataOnUI(myObj);
-        // console.log(data);
+        showDataOnUI(data.data);
     })
     .catch(err=>{
         document.body.innerHTML = document.body.innerHTML + "Something went wrong";
         console.log(err)
     });
-    // localStorage.setItem(emailUser , myObj_serialized);
     userName.value = '';
     email.value = '';
 }
@@ -64,10 +55,14 @@ function showDataOnUI(obj){
     editBtn.textContent = 'Edit';
     editBtn.classList = 'edit button';
 
+    
     item.appendChild(document.createTextNode(visibleText));
     item.appendChild(btn);
     item.appendChild(editBtn);
+    item.innerHTML = item.innerHTML + `<span style='visibility:hidden'>${obj._id}</span>`;
+    // item.innerHTML = item.innerHTML + `<span style='visibility:hidden'>${obj}</span>`;
     ul.appendChild(item);
+    
 
 }
 
@@ -76,16 +71,29 @@ function removeItemFromUI(e){
     e.preventDefault();
 
     if(e.target.classList.contains('button')){
-        let text = e.target.parentNode.firstChild.textContent;
-        let arrDetails = text.split('-');
-        localStorage.removeItem(arrDetails[1]);
+        let text = e.target.parentNode.lastChild.textContent;
+        // console.log(text);
+        axios.delete(`https://crudcrud.com/api/bef2f6a52423441aa70259abb9574616/appintmentData/${text}`)
+        .then(mes => {
+            ul.removeChild(e.target.parentNode);
+            console.log(mes);
+        })
+        .catch(err => console.log((err)))
+
+        // axios.delete("https://crudcrud.com/api/bef2f6a52423441aa70259abb9574616/appintmentData",text)
+        // .then(mes => {
+        //     ul.removeChild(e.target.parentNode);
+        //     console.log(mes);
+        // })
+        // .catch(err => console.log((err)))
+
         
-        // edit functionality
-        if(e.target.classList.contains('edit')){
-            userName.value = arrDetails[0];
-            email.value = arrDetails[1];
-        }
-        ul.removeChild(e.target.parentNode);
+        // // edit functionality
+        // if(e.target.classList.contains('edit')){
+        //     userName.value = arrDetails[0];
+        //     email.value = arrDetails[1];
+        // }
+        // ul.removeChild(e.target.parentNode);
     }
     
 }
