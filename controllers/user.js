f305bfda58267d750ec2f0e8d4ddb900baed2238
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-07-06 23:38:33 +0530

    Q4-LoginScreenFRontend

--- controllers/user.js
+++ controllers/user.js
@@ -4,18 +4,13 @@ const path = require('path');
 const rootDir = path.dirname(process.mainModule.filename);
 
 exports.loadPage = (req, res, next) => {
-    res.sendFile(path.join(rootDir,'views','index.html'));
+    res.sendFile(path.join(rootDir,'views','login.html'));
 }
 
 exports.signUpUser = async (req, res, next) => {
-    // console.log("in sign up ------- ");
 try{
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-
-    // const { name, email, password } = req.body;
-    // console.log(name, email, password);
+    
+    const { name, email, password } = req.body;
     if(name == undefined || name.length === 0 
         || email == undefined || email.length === 0
         || password == undefined || password.length === 0){
@@ -27,5 +22,32 @@ try{
 }
     catch(err){
          res.status(500).json(err);
+    }
+}
+
+exports.loginUser = async (req, res, next) => {
+try{
+    const { email, password } = req.body;
+
+    const details = await User.findAll({
+        where: {
+          email: email
+        }
+      })
+      console.log(details);
+      if (details.length > 0){
+        if(details[0].password === password){
+            res.status(200).json({message: 'Successfully logged in'});
+        }
+        else {
+            res.status(205).json({message: 'incorrect password'});
+        }
+      } else {
+        res.status(206).json({message: 'User not exists! Check your email Id or Signup'});
+      } 
+}
+    catch(err){
+        console.log(err);
+        res.status(500).json(err);
     }
 }
