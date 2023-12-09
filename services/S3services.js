373416b6858138215c0c421a53d288df7d1ac9a1
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-12-10 00:27:13 +0530

    Q20-GoLive

--- services/S3services.js
+++ services/S3services.js
@@ -1,9 +1,7 @@
 const AWS = require('aws-sdk');
 
 exports.uploadToS3 = (data, filename) => {
-    // const BUCKET_NAME = 'expensetrackingapp0809';
-    // const IAM_USER_KEY = 'AKIA6FVZN4FKVZNCUF4N';
-    // const IAM_USER_SECRET = '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';
+
     const BUCKET_NAME = process.env.BUCKET_NAME;
     const IAM_USER_KEY = process.env.IAM_USER_KEY;
     const IAM_USER_SECRET = process.env.IAM_USER_SECRET;
@@ -11,7 +9,6 @@ exports.uploadToS3 = (data, filename) => {
     let s3Bucket = new AWS.S3({
         accessKeyId: IAM_USER_KEY,
         secretAccessKey: IAM_USER_SECRET
-        // Bucket: BUCKET_NAME
     })
 
     var params = {
@@ -19,8 +16,6 @@ exports.uploadToS3 = (data, filename) => {
         Key: filename,
         Body: data,
         ACL: 'public-read'
-        // Access control level
-        // public - read -> automatically readable for puclic use
     }
 
     return new Promise((resolve, reject) => {
